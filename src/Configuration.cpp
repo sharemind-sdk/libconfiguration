sharemind-sdk/libconfiguration

commit d5c6c24cdcd181d6a4665f56066984c6a3f29ec1

Configuration: Fixed current section name in exception message

src/Configuration.cpp
@@ -396,6 +396,7 @@ std::string FileParseJob::ParseState::parseFile(TopLevelParseState<Ptree> & tls,
 {
     constexpr static auto const whitespace = " \t\n\r"_sv;
     std::string line;
+    StringView currentSectionName;
     for (; m_inStream.good(); ++m_lineNumber) {
         std::getline(m_inStream, line);
         if (!m_inStream.good() && !m_inStream.eof())
@@ -427,7 +428,8 @@ std::string FileParseJob::ParseState::parseFile(TopLevelParseState<Ptree> & tls,
             if ((end == StringView::npos)
                 || lv.findFirstNotOf(whitespace, end + 1u) != StringView::npos)
                 throw Configuration::InvalidSyntaxException();
-            auto keyStr(lv.substr(1, end - 1).trimmed(whitespace).str());
+            currentSectionName = lv.substr(1, end - 1).trimmed(whitespace);
+            auto keyStr(currentSectionName.str());
             auto const sectionIt(tls.m_result.find(keyStr));
             if (sectionIt != tls.m_result.not_found()) {
                 assert(sectionIt->second.data()); // Nothing erased yet
@@ -463,7 +465,7 @@ std::string FileParseJob::ParseState::parseFile(TopLevelParseState<Ptree> & tls,
                         throw Configuration::DuplicateKeyException(
                                 concat("Duplicate key \"", std::move(keyStr),
                                        "\" in section [",
-                                       tls.m_result.back().first,
+                                       currentSectionName,
                                        "]! Previous declaration was in \"",
                                        ctx.m_filename->string(), "\" on line ",
                                        ctx.m_lineNumber, '.'));

commit cf4798532fee96c41e0fdcd22a3c7438ee13ca12

Configuration: Fixed invalid memory access
The variable currentSectionName was a string view, pointing to the string held
by variable line. However, when the next line was read, currentSectionName was
left dangling, potentially to invalid memory.

This fix converts the currentSectionName to a regular std::string so it always
keeps a valid copy of the string it is supposed to hold.

The bug was introduced by d5c6c24cdcd181d6a4665f56066984c6a3f29ec1.

src/Configuration.cpp
@@ -396,7 +396,7 @@ std::string FileParseJob::ParseState::parseFile(TopLevelParseState<Ptree> & tls,
 {
     constexpr static auto const whitespace = " \t\n\r"_sv;
     std::string line;
-    StringView currentSectionName;
+    std::string currentSectionName;
     for (; m_inStream.good(); ++m_lineNumber) {
         std::getline(m_inStream, line);
         if (!m_inStream.good() && !m_inStream.eof())
@@ -428,11 +428,12 @@ std::string FileParseJob::ParseState::parseFile(TopLevelParseState<Ptree> & tls,
             if ((end == StringView::npos)
                 || lv.findFirstNotOf(whitespace, end + 1u) != StringView::npos)
                 throw Configuration::InvalidSyntaxException();
-            currentSectionName = lv.substr(1, end - 1).trimmed(whitespace);
+            currentSectionName =
+                    lv.substr(1, end - 1).trimmed(whitespace).str();
             if (currentSectionName.empty()) {
                 tls.m_currentSection = nullptr;
             } else {
-                auto keyStr(currentSectionName.str());
+                auto keyStr(currentSectionName);
                 auto const sectionIt(tls.m_result.find(keyStr));
                 if (sectionIt != tls.m_result.not_found()) {
                     assert(sectionIt->second.data()); // Nothing erased yet